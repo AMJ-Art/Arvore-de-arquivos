0ad5c985a56e404c091ee449121cec0be5328a9f
OctavioFPB <octaviopetry@gmail.com>
2025-05-12 19:23:24 -0300

    ArvoreArq.cpp

--- ArvoreArq.cpp
+++ ArvoreArq.cpp
@@ -2,6 +2,14 @@
 
 using namespace std;
 
+struct Node {
+    string nome;     // Nome da arqv/pasta atual
+    string caminho; //Armazena o camiho das pastas
+    bool eh_pasta; //V ou F
+    long tamanho; // em bytes
+    vector<Node*> filhos;
+};
+
 int main(){
 
     return 0;
